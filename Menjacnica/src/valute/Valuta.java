296017c910ff55d81e58b5b2877c7e170c544d91
jovanmatic12414 <jovan.m.9957@gmail.com>
2016-03-26 22:44:38 +0100

    U okviru obe klase (Valuta i Kurs), dodate metode toString, hashCode i equals.

--- Menjacnica/src/valute/Valuta.java
+++ Menjacnica/src/valute/Valuta.java
@@ -27,4 +27,43 @@ public class Valuta {
 	public void setKursValute(LinkedList<Kurs> kursValute) {
 		this.kursValute = kursValute;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kraciNazivValute == null) ? 0 : kraciNazivValute.hashCode());
+		result = prime * result + ((kursValute == null) ? 0 : kursValute.hashCode());
+		result = prime * result + ((nazivValute == null) ? 0 : nazivValute.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (kraciNazivValute == null) {
+			if (other.kraciNazivValute != null)
+				return false;
+		} else if (!kraciNazivValute.equals(other.kraciNazivValute))
+			return false;
+		if (kursValute == null) {
+			if (other.kursValute != null)
+				return false;
+		} else if (!kursValute.equals(other.kursValute))
+			return false;
+		if (nazivValute == null) {
+			if (other.nazivValute != null)
+				return false;
+		} else if (!nazivValute.equals(other.nazivValute))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Naziv valute: " + nazivValute + " Kraci naziv valute: " + kraciNazivValute;
+	}
 }
