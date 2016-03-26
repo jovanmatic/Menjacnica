d451b12ddcc5fd54bbdccd9f3c8405f23526e4ee
jovanmatic12414 <jovan.m.9957@gmail.com>
2016-03-26 22:38:30 +0100

    Napravljena je klasa Valuta, u okviru paketa valute i klasa Kurs, u okviru paketa kursevi. Dodati atributi u obe klase i odgovarajuce get i set metode.

---
+++ Menjacnica/src/valute/Valuta.java
@@ -0,0 +1,19 @@
+package valute;
+
+public class Valuta {
+	private String nazivValute;
+	private String kraciNazivValute;
+	
+	public String getNazivValute() {
+		return nazivValute;
+	}
+	public void setNazivValute(String nazivValute) {
+		this.nazivValute = nazivValute;
+	}
+	public String getKraciNazivValute() {
+		return kraciNazivValute;
+	}
+	public void setKraciNazivValute(String kraciNazivValute) {
+		this.kraciNazivValute = kraciNazivValute;
+	}
+}
